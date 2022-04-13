322cc620e6da804934d2b96b48d1e165d0cfd912
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-13 18:45:46 +0530

    Uploading LinelengthUc1 file in Uc1_LineLength branch

---
+++ RFPLineComparisionAssignment/RFPLineComparisionAssignment/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RFPLineComparisionAssignment;
+Console.WriteLine("Welcome to Line Comparison Computation Program");
+LineLengthUc1.LineLength();
